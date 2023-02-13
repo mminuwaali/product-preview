1a936923731c1076a32264fea453b3f3d0a67cf6
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2023-02-13 08:43:06 +0100

    final version

--- src/components/cards/detail/index.jsx
+++ src/components/cards/detail/index.jsx
@@ -8,10 +8,13 @@ export default function () {
     return <div className="product-detail">
         <span>perfume</span>
         <h2>gabrielle essence eau de parfum</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt earum mollitia reiciendis eius adipisci consequatur suscipit optio eaque quis nemo.</p>
+        <p>
+            A floral, solar and voluptuous interpretation composed by Olivier Polge,
+            Perfumer-Creator for the House of CHANEL.
+        </p>
         <div className="price">
-            <h3>149.99</h3>
-            <h4>169.99</h4>
+            <h3>$149.99</h3>
+            <h4>$169.99</h4>
         </div>
         <button><img src={cart} alt="" />Add to Cart</button>
     </div>
